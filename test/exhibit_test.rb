faf862f7733ed15ceadaa64b67e3ad6c37d3c948
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-05-27 09:10:59 -0600

    test_it_exists and returns_name test created and run successfully

---
+++ test/exhibit_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/exhibit'
+require './lib/patron'
+require 'pry'
+
+class ExhibitTest < Minitest::Test
+
+  def setup
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
+  def test_it_exists
+    assert_instance_of Exhibit, @exhibit
+  end
+
+  def test_it_returns_name
+    assert_equal "Gems and Minerals", @exhibit.name
+  end
+
+
+
+end
