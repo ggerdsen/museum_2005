49a48cd634e0900342cb57f8809dfbeac4bd2de8
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-05-27 10:06:25 -0600

    create test_it_can_add_interests_for_patron test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -13,7 +13,10 @@ class MuseumTest < Minitest::Test
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
     @patron_1 = Patron.new("Bob", 20)
+    @patron_1.add_interest("Dead Sea Scrolls")
     @patron_1.add_interest("Gems and Minerals")
+    @patron_2 = Patron.new("Sally", 20)
+    @patron_2.add_interest("IMAX")
   end
 
   def test_it_exists
@@ -31,6 +34,25 @@ class MuseumTest < Minitest::Test
     assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.exhibits
     @dmns.add_exhibit(@imax)
     assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
+  end
+
+  def test_it_can_add_interests_for_patron
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @patron_1 = Patron.new("Bob", 20)
+    assert_instance_of Patron, @patron_1
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2 = Patron.new("Sally", 20)
+    assert_instance_of Patron, @patron_2
+    @patron_2.add_interest("IMAX")
+    @dmns.recommend_exhibits(@patron_1)
+
+    assert_equal [@dead_sea_scrolls, @gems_and_minerals], @dmns.recommend_exhibits(@patron_1)
+    @dmns.recommend_exhibits(@patron_2)
+    assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
+
   end
 
 end
