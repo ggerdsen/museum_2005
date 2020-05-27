612661d551b580f54feeccde6836eb5d76b4f105
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-05-27 09:23:42 -0600

    Museum test file created

---
+++ test/museum_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/exhibit'
+require './lib/patron'
+require './lib/museum'
+require 'pry'
+
+class MuseumTest < Minitest::Test
+
+  def setup
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+  end
+
+end
