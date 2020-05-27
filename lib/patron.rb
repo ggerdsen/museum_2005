379ba08f457474cf1edd8f74b21704fb8892948a
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-05-27 09:20:55 -0600

    Patron Class for iteration 1 complete

---
+++ lib/patron.rb
@@ -0,0 +1,18 @@
+class Patron
+
+  attr_reader :name,
+              :money,
+              :interests
+
+  def initialize(name, money)
+    @name = name
+    @money = money
+    @interests = []
+  end
+
+  def add_interest(interest)
+    @interests << interest
+  end
+
+
+end
