e09dfcb8acb84aa16807e158d71f2c2647c55463
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-05-27 10:21:33 -0600

    added test_it_can_add_patrons test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -54,4 +54,25 @@ class MuseumTest < Minitest::Test
 
   end
 
+  def test_it_can_admit_patrons
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    assert_equal [], @dmns.patrons
+    @patron_1 = Patron.new("Bob", 20)
+    assert_instance_of Patron, @patron_1
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_2 = Patron.new("Sally", 20)
+    assert_instance_of Patron, @patron_2
+    @patron_2.add_interest("Dead Sea Scrolls")
+    @patron_3 = Patron.new("Johnny", 5)
+    assert_instance_of Patron, @patron_3
+    @patron_3.add_interest("Dead Sea Scrolls")
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+    assert_equal [@patron_1, @patron_2, @patron_3], @dmns.patrons
+  end
+
 end
